d80af99eb0441e65a92a1a42d28f54586d5a989e
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-07-15 00:28:41 -0400

    day 1

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,22 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside", "Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("foyer", "Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("overlook", "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("narrow", "Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("treasure", "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -39,6 +39,9 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('John')
+player.location = room['outside']
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,22 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+inp = ''
+while inp != 'q':
+    print("\n"+player.location.fullname+ " - " +player.location.description+"\n ")
+    print("Enter direction (n s e w) or q to quit")
+
+    try:
+        inp = input()
+        if inp == 'n':
+            player.location = room[player.location.name].n_to
+        elif inp == 's':
+            player.location = room[player.location.name].s_to
+        elif inp == 'e':
+            player.location = room[player.location.name].e_to
+        elif inp == 'w':
+            player.location = room[player.location.name].w_to
+    except:
+        print("Invalid direction")
+    
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+  def __init__(self, name, fullname, description):
+    self.name = name
+    self.fullname = fullname
+    self.description = description
